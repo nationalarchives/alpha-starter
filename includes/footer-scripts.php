aceff6fd65fcd71151d17eec3959fd22012ebae4
Gwyn Jones <m@gwnjns.com>
2015-10-05 12:07:08 +0100

    Addition of basic HTML with boilerplate extracted to includes.

---
+++ includes/footer-scripts.php
@@ -0,0 +1,28 @@
+<!--[if (gte IE 6)&(lte IE 8)]>
+<script src="http://livelb.nationalarchives.gov.uk/wp-content/themes/tna/scripts/jquery-1.10.2.min.js"></script>
+<script src="http://livelb.nationalarchives.gov.uk/wp-content/themes/tna/scripts/jquery-migrate-1.0.0.js"></script>
+<![endif]-->
+
+<!--[if gte IE 9]><!-->
+<script src="http://www.nationalarchives.gov.uk/wp-content/themes/tna/scripts/jquery-2.0.3.min.js"></script>
+<!--<![endif]-->
+
+
+<script src="http://www.nationalarchives.gov.uk/wp-content/themes/tna/scripts/backstretch.js"></script>
+
+
+<script src="https://code.jquery.com/ui/1.11.2/jquery-ui.min.js"></script>
+
+
+<script src="http://www.nationalarchives.gov.uk/wp-content/themes/tna/scripts/tna-definitions.js"></script>
+<script src="http://www.nationalarchives.gov.uk/wp-content/themes/tna/scripts/tna-bindings.js"></script>
+<script src="http://www.nationalarchives.gov.uk/wp-content/themes/tna/scripts/tna-run-on-page-load.js"></script>
+
+
+<script src="http://www.nationalarchives.gov.uk/scripts/footer-img.js" type="text/javascript"></script>
+
+
+<script type='text/javascript'
+        src='http://www.nationalarchives.gov.uk/wp-content/themes/tna/js/navigation.js?ver=20120206'></script>
+<script type='text/javascript'
+        src='http://www.nationalarchives.gov.uk/wp-content/themes/tna/js/skip-link-focus-fix.js?ver=20130115'></script>
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en-gb">
+<?php include 'includes/head.php'; ?>
+
+<body>
+<?php include 'includes/header.php'; ?>
+
+<div class="container" id="page_wrap" role="main">
+    <?php include 'includes/breadcrumb.php'; ?>
+    <div class="row">
+        <div class="col ends-at-full box clr">
+            <div class="heading-holding-banner">
+                <h2><span><span>...</span></span></h2>
+            </div>
+            <div class="breather">
+                <p>...</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<a id="goTop"></a>
+
+<?php include 'includes/footer.php'; ?>
+<?php include 'includes/footer-scripts.php'; ?>
+
+</body>
+</html>
